e4c2f64e70b3cb99cd50dfc0f9f8ccac0c71d6f5
Arnaud <a.avaullee@gmail.com>
2023-12-12 09:34:33 +0100

    FIX : Change relative path in createApp.js file (ISSUE #1)

--- src/createApp.js
+++ src/createApp.js
@@ -17,7 +17,7 @@ export async function createApp() {
     app.use(express.urlencoded({ extended: true }))
 
     app.set("view engine", "ejs")
-    app.set("views", "src/views")
+    app.set("views", "./src/views")
 
     app.get("/home", async (req, res) => {
         let recipes = await DbRecipe.find()
